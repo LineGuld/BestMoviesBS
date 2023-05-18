91b59f1b66588f08614407ecdae7d1d065751504
Line Kirstine Guld <304432@VIAUC.DK>
2023-05-18 22:32:13 +0200

    fixed so array works

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using BestMoviesBS.DataAccess;
 using BestMoviesBS.Services;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+});;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
